59e6b0b0e3ca986208542af337dd7309d758e029
Chesli Jones <chesli@croner.com>
2022-02-09 15:55:55 -0800

    Checking in work

--- api/src/core/tests.py
+++ api/src/core/tests.py
@@ -1,24 +1,8 @@
 import unittest
-from django.test import TestCase
+from unittest.mock import patch
 
 from .utils import RedisUtil
 from .views import KeysViewSet
-
-class RedisUtilTestCase(unittest.TestCase):
-    def setUp(self):
-        self.redis_util = RedisUtil()
-
-    def test_create(self):
-        pass
-
-    def test_destroy(self):
-        pass
-
-    def test_retrieve(self):
-        pass
-
-    def tearDown(self):
-        del self.redis_util
 
 class KeysViewSetTestCase(unittest.TestCase):
     def setUp(self):
@@ -35,3 +19,25 @@ class KeysViewSetTestCase(unittest.TestCase):
 
     def tearDown(self):
         del self.redis_util
+
+# class RedisUtilTestCase(unittest.TestCase):
+#     def setUp(self):
+#         self.redis_util = RedisUtil()
+
+#     def test_create(self):
+#         with patch('utils.redis.StrictRedis.set') as mocked_set:
+#             mocked_set.return_value
+#         # self.redis_util.create({'key': 'hello', 'value': 'world'})
+#         # self.assertEqual(self.redis_util.retrieve('hello'), 'world')
+
+#     # def test_destroy(self):
+#     #     self.redis_util.create({'key': 'hello', 'value': 'world'})
+#     #     self.assertEqual(self.redis_util.destroy('hello'), 1)
+
+#     # def test_retrieve(self):
+#     #     self.redis_util.create({'key': 'hello', 'value': 'world'})
+#     #     self.assertEqual(self.redis_util.retrieve('hello'), 'world')
+
+#     def tearDown(self):
+#         del self.redis_util
+
--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,11 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import {rest} from 'msw';
+import {setupServer} from 'msw/node';
+
+// const server = setupServer(
+
+// )
+
 test('Create component renders', () => {
   render(<App />);
   const createComponent = screen.getByText(/Store Value at Given Key/i);
   expect(createComponent).toBeInTheDocument();
 });
+
+
 
 test('Retrieve component renders', () => {
   render(<App />);
